c4b8b7600ed54cd8cf1f2c1cac8b23c4281a166f
Rasoft <danil.taran1999@gmail.com>
2018-11-27 00:55:02 +0300

    finished main index page

---
+++ main.js
@@ -0,0 +1,15 @@
+var open_button = document.getElementById("navbar-toggle-open");
+var close_button = document.getElementById("navbar-toggle-close");
+var navbar_list = document.getElementById("navbar-list");
+
+open_button.addEventListener("click",function () {
+    this.classList.toggle("disp-none");
+    close_button.classList.toggle("disp-none");
+    navbar_list.classList.toggle("disp-none");
+});
+
+close_button.addEventListener("click",function () {
+    this.classList.toggle("disp-none");
+    open_button.classList.toggle("disp-none");
+    navbar_list.classList.toggle("disp-none");
+});
