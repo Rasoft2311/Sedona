e7cde7eaff80233a05b834d936b356a1b959caca
Rasoft <danil.taran1999@gmail.com>
2018-11-27 17:21:44 +0300

    fixed some toggler action

--- main.js
+++ main.js
@@ -5,11 +5,11 @@ var navbar_list = document.getElementById("navbar-list");
 open_button.addEventListener("click",function () {
     this.classList.toggle("disp-none");
     close_button.classList.toggle("disp-none");
-    navbar_list.classList.toggle("disp-none");
+    navbar_list.classList.toggle("disp-none-mobile");
 });
 
 close_button.addEventListener("click",function () {
     this.classList.toggle("disp-none");
     open_button.classList.toggle("disp-none");
-    navbar_list.classList.toggle("disp-none");
+    navbar_list.classList.toggle("disp-none-mobile");
 });
